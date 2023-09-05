b669097561075afc0f041aede5515aab7b51939d
Ivar <ivarrobot89@gmail.com>
2023-09-05 11:40:37 +0200

    empezando proyecto

--- TMB_CSHARP/Program.cs
+++ TMB_CSHARP/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// Recordando el uso de C#
+
+/* Calculadora basal
+ * by Ivar ca
+*/
+
+
+
+using System;
+
+
+
+class Program
+{
+
+    static void Main()
+    {
+
+        int n2, n3;
+        double n1;
+        double resultado;
+
+        Console.Write("Dime tu peso en kg : ");
+        n1 = double.Parse(Console.ReadLine());
+        Console.Write("Dime tu altura en cm : ");
+        n2 = int.Parse(Console.ReadLine());
+        Console.Write("Dime tu edad : ");
+        n3 = int.Parse(Console.ReadLine());
+
+        resultado = (10 * n1) + (6.25 * n2) - (5 * n3) + 5;
+
+        Console.WriteLine("El resultado es {0}", resultado);
+        Console.WriteLine("Gracias por usar mi calculadora");
+
+
+
+
+    }
+
+
+
+}
